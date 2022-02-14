d3180fcb95e47dcbb62d809497e299730ac65584
Vrashti <vrashti@localhost.localdomain>
2022-02-15 03:31:08 +0530

    First Commit

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j=1,num;
+	printf("Enter A Number:");
+	scanf("%d",&num);
+
+	for(i=1;i<=num;i++)
+		j=j*i;
+
+	printf("Factorial of %d is %d",num,j);
+}
---
+++ target.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int balance=100;
+	int target=1000;
+	float rate=2.0;
+	int year=0;
+	do
+	{
+		float interest=balance*rate;
+		balance=balance+interest;
+		year++;
+	}while(balance<target);
+	printf("%d No. of years to achieve balance %d.\n",year,balance);
+	return 0;
+}
+
+	
